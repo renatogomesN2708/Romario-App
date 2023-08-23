739c6adc106d7b9edafec99fb57d9003f01f043b
renatogomesN2708 <wellenvitoria.1904@gmail.com>
2023-08-23 20:55:34 -0300

    first commit

---
+++ src/components/sections/login/Login.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './index.scss'
+import check from '../../../assets/check.png'
+import arrow from '../../../assets/aroow.png'
+
+
+
+
+const Login = () => {
+  return (
+    <section className='div'>
+        <div className='icon'>
+            <img src={check} alt="" />
+            <img id='arrow' src={arrow} alt="" />
+       
+            <h1 className='confir'>acesso Confirmado</h1>
+            <h1 className='name'><span>Romário Andrade</span></h1>
+        </div>
+    </section>
+  )
+}
+
+export default Login
---
+++ src/view/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './index.scss'
+
+import Login from '../components/sections/login/Login'
+
+
+const Home = () => {
+  return (
+    <div className='home'>
+        <Login/>
+    </div>
+  )
+}
+
+export default Home
